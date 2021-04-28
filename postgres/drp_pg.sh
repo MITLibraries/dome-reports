ed589565babe4eff791b6f1ca5b4016aa4f85c8f
carlj11 <carlj11@users.noreply.github.com>
2021-04-28 01:34:06 -0400

    Add remote Postgres database connection URL
    Run remote psql queries against Postgres database server

--- postgres/drp_pg.sh
+++ postgres/drp_pg.sh
@@ -1,22 +1,23 @@
 #!/usr/bin/sh
-# bash script to query Dome's Postgres DB
+# bash script to remotely query Dome's (dome.mit.edu) Postgres DB
 # for monthly report data
+
+# Amazon AWS Database server: dome6-prod-rds-prod.cvvzvytwmb4n.us-east-1.rds.amazonaws.com
+# remote SQL queries must be run from: domeanalytics-prod.mitlib.net
 
 YYMM=`date +"%y%m"`
 COMM='comm-'$YYMM'.tsv'
 COLL='coll-'$YYMM'.tsv'
 ITC='itc-'$YYMM'.tsv'
 
-#    Note:  these psql calls are not configured
-#           for a remote connection
+#    Note:  these psql calls require manually entered password
 #
-
 
 #    creates tab-delimited rows
 #    no headers or footer ( -t )
 #
-#    to import into sqlite, create a test database:
-#        >sqlite3 test-drp-4.db 
+#    to import into sqlite database: 
+#        >sqlite3 drp-prod.db 
 #
 #
 #    then run these commands at the sqlite3 command line
@@ -25,26 +26,20 @@ ITC='itc-'$YYMM'.tsv'
 #        .mode tabs 
 #
 
-#  -- extract communities
-# preliminary version where tabs are specified
-# this does not work on the target server, so the next version below
-# settles for the default vertical bar char '|'
+# use the Postgres default separator - the vertical bar '|'
 # the metadata_field_id can vary from one DSpace implementation to another
 # the -t flag in psql suppresses column headers and footers
 
-sudo -u postgres psql -d dspace -AF \$'\t' --no-align -t -c 'SELECT comm.uuid, mv.text_value as name, mv.text_value as short_name FROM community comm INNER JOIN metadatavalue mv ON comm.uuid = mv.dspace_object_id WHERE mv.metadata_field_id = 64;' > $COMM
+#  -- extract communities
 
-# working version using the Postgres default separator - the vertical bar '|'
-sudo -u postgres psql -d dspace --no-align -t -c 'SELECT comm.uuid, mv.text_value as name, mv.text_value as short_name FROM community comm INNER JOIN metadatavalue mv ON comm.uuid = mv.dspace_object_id WHERE mv.metadata_field_id = 64;' > $COMM
+psql -h dome6-prod-rds-prod.cvvzvytwmb4n.us-east-1.rds.amazonaws.com -U dspace -d dome6prod --no-align -t -c 'SELECT comm.uuid, mv.text_value as name, mv.text_value as short_name FROM community comm INNER JOIN metadatavalue mv ON comm.uuid = mv.dspace_object_id WHERE mv.metadata_field_id = 64;' > $COMM
+
 
 #  -- extract collections
-# here, the working version with the default field separator 
 
-sudo -u postgres psql -d dspace --no-align -t -c 'SELECT col.uuid, c2c.community_id, mv.text_value as name, mv.text_value as short_name FROM community2collection c2c INNER JOIN collection col ON c2c.collection_id = col.uuid INNER JOIN metadatavalue mv ON col.uuid = mv.dspace_object_id WHERE mv.metadata_field_id = 64;' > $COLL
+psql -h dome6-prod-rds-prod.cvvzvytwmb4n.us-east-1.rds.amazonaws.com -U dspace -d dome6prod --no-align -t -c 'SELECT col.uuid, c2c.community_id, mv.text_value as name, mv.text_value as short_name FROM community2collection c2c INNER JOIN collection col ON c2c.collection_id = col.uuid INNER JOIN metadatavalue mv ON col.uuid = mv.dspace_object_id WHERE mv.metadata_field_id = 64;' > $COLL
 
 
 #  --extract counts
-# here, the workiing version with the default field separator
 
-sudo -u postgres psql -d dspace --no-align -t -c 'SELECT coll.uuid, EXTRACT(YEAR FROM CURRENT_TIMESTAMP), EXTRACT(MONTH FROM CURRENT_TIMESTAMP), COUNT(item.uuid) FROM collection coll INNER JOIN item ON coll.uuid = item.owning_collection WHERE item.in_archive = true and item.withdrawn = false  GROUP BY coll.uuid;' > $ITC
-
+psql -h dome6-prod-rds-prod.cvvzvytwmb4n.us-east-1.rds.amazonaws.com -U dspace -d dome6prod --no-align -t -c 'SELECT coll.uuid, EXTRACT(YEAR FROM CURRENT_TIMESTAMP), EXTRACT(MONTH FROM CURRENT_TIMESTAMP), COUNT(item.uuid) FROM collection coll INNER JOIN item ON coll.uuid = item.owning_collection WHERE item.in_archive = true and item.withdrawn = false  GROUP BY coll.uuid;' > $ITC
