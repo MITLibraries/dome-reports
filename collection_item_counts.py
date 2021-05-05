e723b6c555150a6050794da0a2780feffbd938b7
W. Hays <1410306+wlhays@users.noreply.github.com>
2021-05-05 10:57:37 -0700

    added xlsx styling function to adjust bolding, alignment and widths

--- collection_item_counts.py
+++ collection_item_counts.py
@@ -11,10 +11,12 @@ from sys import argv, exit
 #from xhtml2pdf import pisa
 from contextlib import closing
 
+from openpyxl import load_workbook
+from openpyxl.styles import Alignment, Font, NamedStyle, Border, Side
 
 def main(argv):
 
-    db_filepath = "drp-prod.db"
+    db_filepath = "drp.db"
 
     # month is used only in the names of the report files
     month = year = 0
@@ -34,7 +36,7 @@ def main(argv):
     # Commandline options
     try:
         opts, args = getopt.getopt(argv,"hy:f:d:o:",
-                  ["year=","fmts=","db=","output_dir"])
+                  ["year=","fmts=","db=","output_dir="])
     except getopt.GetoptError:
         print_help()
         exit(2)
@@ -106,10 +108,10 @@ def main(argv):
     #get array of corresponding months
     pt.columns = [calendar.month_abbr[i] for i in ar_mo_int]
 
-    # add column totals as last row
-    totals = pt.sum(numeric_only=True, axis=0)
-    totals.name = ("Totals", "")
-    pt = pt.append(totals)
+    #add column totals
+    sums =  pt.sum(numeric_only=True, axis=0)   #this is a Series
+    sums.name = ("Totals", "")
+    pt = pt.append(sums)
 
     rpt_filepath = f'{output_dir}/drp_col_it_{year}-{month}'
 
@@ -132,8 +134,11 @@ def main(argv):
             md_out.write(format_markdown(table, year))
             logging.info(f"created: {rpt_filepath}.md") 
     if XLSX:
-        pt.to_excel(f'{rpt_filepath}.xlsx', index = True)
+        pt.to_excel(f'{rpt_filepath}.xlsx')   
         logging.info(f"created: {rpt_filepath}.xlsx") 
+
+        style_xlsx(f'{rpt_filepath}.xlsx', len(ar_mo_int), len(pt) + 1)
+        logging.info(f'restyled excel report {rpt_filepath}.xlsx')
 
     # print to console
     if SCREEN:
@@ -161,6 +166,37 @@ def add_common_header(table, year):
     return f"""\n    MIT Libraries  -- dome.mit.edu\n
     Monthly Reports -- {year}\n    Collection Item Counts\n\n""" \
     + table + "\n"
+
+def style_xlsx(excel_filename, ncols, nrows):
+
+
+    c_style = NamedStyle(name="c_style")
+    c_style.font = Font(bold=False)
+    c_style.alignment = Alignment(horizontal="left", vertical="top", wrap_text=True)
+
+    wb = load_workbook(filename = excel_filename)
+    ws = wb.active
+
+    ws.column_dimensions['A'].width = 31
+    ws.column_dimensions['B'].width = 38
+
+    for col in ["A", "B"]:
+        for cell in ws[col]:
+            cell.style = c_style
+
+    # put the first two col headers back to bold
+    ws["A1"].font = Font(bold=True)
+    ws["B1"].font = Font(bold=True)
+
+    # add border line above totals
+    month_cols = [chr(x) for x in range(67, 67+ncols )]
+    cells = [month_cols[i] + str(nrows) for i in range(0,ncols)]
+    double = Side(border_style="medium", color="000000")
+
+    for cell in cells:
+        ws[cell].border = Border(top=double)
+
+    wb.save(filename=excel_filename)
 
 # community and collection are presented tuple
 # tabulate and panda docs don't offer an option to break up as columns
