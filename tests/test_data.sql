7414c9f536f1e6c8aa92aced10a3558ec5d5dd95
W. Hays <1410306+wlhays@users.noreply.github.com>
2021-07-31 14:23:43 -0700

    Adjusted test file names (to .csv), the test sql script and instructions.

--- tests/test_data.sql
+++ tests/test_data.sql
@@ -7,22 +7,22 @@ PRAGMA foreign_keys = ON;
 
 BEGIN TRANSACTION;
 
-INSERT INTO Community(uuid, name, short_name) VALUES('aaaaaaaa-e361-4685-a048-5f97bd08c466','Rotch Library Architecture','Rotch Library Architecture');
-INSERT INTO Community(uuid, name, short_name) VALUES('bbbbbbbb-6115-4272-9487-429c1a45c93e','Digital Technology','Digital Technology');
-INSERT INTO Community(uuid, name, short_name) VALUES('cccccccc-5a18-434a-a3ba-a001ef186b5f','MIT Libraries','MIT Libraries');
-INSERT INTO Community(uuid, name, short_name) VALUES('dddddddd-ba85-4fdc-94c8-fd13048cbd1d','Lewis Music Library','Lewis Music Library');
-INSERT INTO Community(uuid, name, short_name) VALUES('eeeeeeee-9843-4996-b590-8182f885c833','Visual Collections','Visual Collections');
-INSERT INTO Community(uuid, name, short_name) VALUES('ffffffff-9843-4996-b590-8182f885c8ff','Dome Reports','Dome Reports');
+INSERT INTO Community(uuid, name, short_name, handle) VALUES('aaaaaaaa-e361-4685-a048-5f97bd08c466','Rotch Library Architecture','Rotch Library Architecture','1229.3/871');
+INSERT INTO Community(uuid, name, short_name, handle) VALUES('bbbbbbbb-6115-4272-9487-429c1a45c93e','Digital Technology','Digital Technology','1229.3/872');
+INSERT INTO Community(uuid, name, short_name, handle) VALUES('cccccccc-5a18-434a-a3ba-a001ef186b5f','MIT Libraries','MIT Libraries','1229.3/873');
+INSERT INTO Community(uuid, name, short_name, handle) VALUES('dddddddd-ba85-4fdc-94c8-fd13048cbd1d','Lewis Music Library','Lewis Music Library','1229.3/874');
+INSERT INTO Community(uuid, name, short_name, handle) VALUES('eeeeeeee-9843-4996-b590-8182f885c833','Visual Collections','Visual Collections','1229.3/875');
+INSERT INTO Community(uuid, name, short_name, handle) VALUES('ffffffff-9843-4996-b590-8182f885c8ff','Dome Reports','Dome Reports','1229.3/876');
 
-INSERT INTO Collection(uuid, comm_uuid, name, short_name, reportable) VALUES('7b1d1752-9843-4996-b590-8182f885c833','eeeeeeee-9843-4996-b590-8182f885c833','Aga Khan Visual Archive','Aga Khan Visual Archive', 1);
-INSERT INTO Collection(uuid, comm_uuid, name, short_name, reportable) VALUES('a5911166-e361-4685-a048-5f97bd08c466','aaaaaaaa-e361-4685-a048-5f97bd08c466','Perceptual Form of the City','Perceptual Form of the City', 1);
-INSERT INTO Collection(uuid, comm_uuid, name, short_name, reportable) VALUES('e129658d-19f4-4a11-99f0-ce25b746bae5','eeeeeeee-9843-4996-b590-8182f885c833','Frankel Built Landscape Archive','Frankel Built Landscape Archive', 1);
-INSERT INTO Collection(uuid, comm_uuid, name, short_name, reportable) VALUES('67d5e318-04fb-4840-954b-dd69c6c5ec2c','eeeeeeee-9843-4996-b590-8182f885c833','Research Photography by Jean Jackson','Research Photography by Jean Jackson', 1);
-INSERT INTO Collection(uuid, comm_uuid, name, short_name, reportable) VALUES('62206c51-ba85-4fdc-94c8-fd13048cbd1d','dddddddd-ba85-4fdc-94c8-fd13048cbd1d','Inventions of Note','Inventions of Note', 1);
-INSERT INTO Collection(uuid, comm_uuid, name, short_name, reportable) VALUES('ec2ce98d-6115-4272-9487-429c1a45c93e','bbbbbbbb-6115-4272-9487-429c1a45c93e','Project Whirlwind Reports','Project Whirlwind Reports', 1);
-INSERT INTO Collection(uuid, comm_uuid, name, short_name, reportable) VALUES('929c8db8-d461-430c-9ce9-9356719e5c36','eeeeeeee-9843-4996-b590-8182f885c833','Reconstruction of Harvard Bridge','Reconstruction of Harvard Bridge', 1);
-INSERT INTO Collection(uuid, comm_uuid, name, short_name, reportable) VALUES('a6f7a9c6-5a18-434a-a3ba-a001ef186b5f','cccccccc-5a18-434a-a3ba-a001ef186b5f','DSpace 7 Test Everything','DSpace 7 Test Everything', 1);
-INSERT INTO Collection(uuid, comm_uuid, name, short_name, reportable) VALUES('b4f7a888-5a18-434a-83ba-a001ef678954','ffffffff-9843-4996-b590-8182f885c8ff', 'Monthly Collection Item Reports', 'Monthly Collection Items', 0);
+INSERT INTO Collection(uuid, comm_uuid, name, short_name, reportable, handle) VALUES('7b1d1752-9843-4996-b590-8182f885c833','eeeeeeee-9843-4996-b590-8182f885c833','Aga Khan Visual Archive','Aga Khan Visual Archive', 1,'1229.3/8714s');
+INSERT INTO Collection(uuid, comm_uuid, name, short_name, reportable, handle) VALUES('a5911166-e361-4685-a048-5f97bd08c466','aaaaaaaa-e361-4685-a048-5f97bd08c466','Perceptual Form of the City','Perceptual Form of the City', 1,'1229.3/8715');
+INSERT INTO Collection(uuid, comm_uuid, name, short_name, reportable, handle) VALUES('e129658d-19f4-4a11-99f0-ce25b746bae5','eeeeeeee-9843-4996-b590-8182f885c833','Frankel Built Landscape Archive','Frankel Built Landscape Archive', 1,'1229.3/8716');
+INSERT INTO Collection(uuid, comm_uuid, name, short_name, reportable, handle) VALUES('67d5e318-04fb-4840-954b-dd69c6c5ec2c','eeeeeeee-9843-4996-b590-8182f885c833','Research Photography by Jean Jackson','Research Photography by Jean Jackson', 1,'1229.3/8717');
+INSERT INTO Collection(uuid, comm_uuid, name, short_name, reportable, handle) VALUES('62206c51-ba85-4fdc-94c8-fd13048cbd1d','dddddddd-ba85-4fdc-94c8-fd13048cbd1d','Inventions of Note','Inventions of Note', 1,'1229.3/8718');
+INSERT INTO Collection(uuid, comm_uuid, name, short_name, reportable, handle) VALUES('ec2ce98d-6115-4272-9487-429c1a45c93e','bbbbbbbb-6115-4272-9487-429c1a45c93e','Project Whirlwind Reports','Project Whirlwind Reports', 1,'1229.3/8719');
+INSERT INTO Collection(uuid, comm_uuid, name, short_name, reportable, handle) VALUES('929c8db8-d461-430c-9ce9-9356719e5c36','eeeeeeee-9843-4996-b590-8182f885c833','Reconstruction of Harvard Bridge','Reconstruction of Harvard Bridge', 1,'1229.3/8720');
+INSERT INTO Collection(uuid, comm_uuid, name, short_name, reportable, handle) VALUES('a6f7a9c6-5a18-434a-a3ba-a001ef186b5f','cccccccc-5a18-434a-a3ba-a001ef186b5f','DSpace 7 Test Everything','DSpace 7 Test Everything', 1,'1229.3/8721');
+INSERT INTO Collection(uuid, comm_uuid, name, short_name, reportable, handle) VALUES('b4f7a888-5a18-434a-83ba-a001ef678954','ffffffff-9843-4996-b590-8182f885c8ff', 'Monthly Collection Item Reports', 'Monthly Collection Items', 0,'1229.3/8722');
 
 INSERT INTO Monthly_Item_Count VALUES('7b1d1752-9843-4996-b590-8182f885c833',2021,1,33105);
 INSERT INTO Monthly_Item_Count VALUES('7b1d1752-9843-4996-b590-8182f885c833',2021,2,33110);
